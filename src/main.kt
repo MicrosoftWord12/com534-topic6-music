5e4c9d3298c945a841430eeed1f017343043ea12
Microsft-Wrd <mrjacobbrooks@gmail.com>
2025-10-27 13:45:19 +0000

    feat: Album, Song and Singles are all working in the way that is required. However adding to the singles list isn't quite how its meant to be but it still works non the less

--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,18 @@
 fun main() {
+    val listOfSongs = mutableListOf<Song>()
+    listOfSongs.add(Song("One More Time", "Imagine Dragons", 2.5))
+    listOfSongs.add(Song("Song", "Michael Jackson", 5.0))
 
+    val singles = mutableListOf<Single>()
+    val album = Album(listOfSongs)
+
+
+
+    for (song in album.getAllSongs()) {
+        singles.add(Single(song, song))
+    }
+
+    for (single in singles) {
+        println(single.getPlayingTime())
+    }
 }
