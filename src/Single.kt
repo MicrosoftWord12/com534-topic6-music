cb976e23a0e6d842ccc4812b9a04d26d56f5dfcf
Microsft-Wrd <mrjacobbrooks@gmail.com>
2025-10-27 13:36:24 +0000

    feat: Single class with the implemented methods derived from the IMusic Interface

--- src/Single.kt
+++ src/Single.kt
@@ -1,2 +1,17 @@
-class Single(val aSide: Song, val bSide: Song) {
+import types.interfaces.IMusic
+
+class Single(val aSide: Song, val bSide: Song): IMusic {
+
+    override fun getAllSongs(): List<Song> {
+        val newList = mutableListOf<Song>()
+
+        newList.add(aSide)
+        newList.add(bSide)
+        return newList.toList()
+    }
+
+    override fun getPlayingTime(): Double {
+        return aSide.playingTime + bSide.playingTime
+    }
+
 }
