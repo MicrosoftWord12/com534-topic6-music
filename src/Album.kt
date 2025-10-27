1d0a56de555adc8542cf6f27dd3336d96f491282
Microsft-Wrd <mrjacobbrooks@gmail.com>
2025-10-27 13:40:27 +0000

    feat: Album class now has the implemented methods of getAllSongs and getPlayingTime

--- src/Album.kt
+++ src/Album.kt
@@ -1,6 +1,24 @@
-class Album(val songs: List<Song>) {
+import types.interfaces.IMusic
+
+class Album(val songs: List<Song>): IMusic {
 
     fun filterSongs(author: String): List<Song> {
         return songs.filter { it.artist == author}
     }
+
+    override fun getAllSongs(): List<Song> {
+        return songs
+    }
+
+    override fun getPlayingTime(): Double {
+        var totalPlayingTime = 0.0
+
+        for (song in songs) {
+            totalPlayingTime += song.playingTime
+        }
+
+        return totalPlayingTime
+    }
+
+
 }
