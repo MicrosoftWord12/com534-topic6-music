e6fb596dbb837991df65ff06679248644a3752c0
2brooj75 <2brooj75@solent.ac.uk>
2025-10-31 10:17:40 +0000

    chore: Rewrite the Single#getAllSongs method to return directly a listOf(aSide, bSide) methods

--- src/Single.kt
+++ src/Single.kt
@@ -3,11 +3,13 @@ import types.interfaces.IMusic
 class Single(val aSide: Song, val bSide: Song): IMusic {
 
     override fun getAllSongs(): List<Song> {
-        val newList = mutableListOf<Song>()
+        return listOf(aSide, bSide)
 
-        newList.add(aSide)
-        newList.add(bSide)
-        return newList.toList()
+//        val newList = mutableListOf<Song>()
+//
+//        newList.add(aSide)
+//        newList.add(bSide)
+//        return newList.toList()
     }
 
     override fun getPlayingTime(): Double {
